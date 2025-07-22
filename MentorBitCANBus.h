c80b57d802282ed52bdfbb77f1ed7a0d28ab305c
krosDC <kevin.ingenieria@digitalcodesign.com>
2025-07-22 08:41:47 +0100

    Added basic code to header file.

---
+++ MentorBitCANBus.h
@@ -0,0 +1,54 @@
+/*
+
+            ██████╗    ██╗    ██████╗    ██╗   ████████╗    █████╗    ██╗               
+            ██╔══██╗   ██║   ██╔════╝    ██║   ╚══██╔══╝   ██╔══██╗   ██║               
+            ██║  ██║   ██║   ██║  ███╗   ██║      ██║      ███████║   ██║               
+            ██║  ██║   ██║   ██║   ██║   ██║      ██║      ██╔══██║   ██║               
+            ██████╔╝   ██║   ╚██████╔╝   ██║      ██║      ██║  ██║   ███████╗          
+            ╚═════╝    ╚═╝    ╚═════╝    ╚═╝      ╚═╝      ╚═╝  ╚═╝   ╚══════╝          
+                                                                                        
+     ██████╗    ██████╗    ██████╗    ███████╗   ███████╗   ██╗    ██████╗    ███╗   ██╗
+    ██╔════╝   ██╔═══██╗   ██╔══██╗   ██╔════╝   ██╔════╝   ██║   ██╔════╝    ████╗  ██║
+    ██║        ██║   ██║   ██║  ██║   █████╗     ███████╗   ██║   ██║  ███╗   ██╔██╗ ██║
+    ██║        ██║   ██║   ██║  ██║   ██╔══╝     ╚════██║   ██║   ██║   ██║   ██║╚██╗██║
+    ╚██████╗   ╚██████╔╝   ██████╔╝   ███████╗   ███████║   ██║   ╚██████╔╝   ██║ ╚████║
+     ╚═════╝    ╚═════╝    ╚═════╝    ╚══════╝   ╚══════╝   ╚═╝    ╚═════╝    ╚═╝  ╚═══╝ 
+        
+
+    Autor: Digital Codesign
+    Version: 1.0.0
+    Fecha de creación: Enero de 2024
+    Fecha de version: Enero de 2024
+    Repositorio: https://github.com/DigitalCodesign/MentorBit-CANBus
+    Descripcion: 
+        Esta libreria esta especificamente diseñada para ser utilizada junto con 
+        el modulo MentorBit CAN
+    Metodos principales:
+        MentorBitCANBus -> constructor de la clase
+        begin -> inicializador, debe colocarse en el setup
+
+    TODO: CONFIRM_BUFFER_BYTES
+    TODO: SEND_I2C_BUFFER
+    TODO: CHANGE_CAN_BITRATE
+    TODO: CHANGE_I2C_ADDRESS
+    TODO: SEND_BYTES_TO_CAN
+
+*/
+
+#ifndef MentorBitCANBus_h
+#define MentorBitCANBus_h
+
+#include <Wire.h>
+
+class MentorBit_CANBus
+{
+
+    public:
+
+
+    private:
+
+
+}
+
+#endif
